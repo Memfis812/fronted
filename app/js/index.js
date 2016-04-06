d328cd8b86fbb40700410da28626e334dad76c43
Konstantin Inkin <Konstantin.inkin@mail.ru>
2016-04-05 19:27:33 -0700

    js_homework_1

---
+++ app/js/index.js
@@ -0,0 +1,37 @@
+//Задача 1(периметр)
+var a = 5;
+var b = 10;
+var p = 2 * (a + b);
+console.log(p)
+
+//Задача 1(площадь)
+var a = 5;
+var b = 10;
+var s = a * b;
+console.log(s)
+
+//Задача 2
+var x1 = 2;
+var y1 = 6;
+var x2 = 10;
+var y2 = 20;
+var s = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow(y2 - y1), 2));
+console.log(s)
+
+//Задача 3
+var r1 = 10;
+var r2 = 15;
+var p = 3.14;
+var s = p * (Math.pow(r2, 2) -  Math.pow(r1, 2));
+console.log(s)
+
+//Задача 4
+var a = 3;
+var b = 2;
+var c = 1;
+if ((a != b) && (a != c) && (b != c)) {
+console.log('Все цифры данного трехзначного числа не равны')
+} else {
+console.log('Все цифры равны')
+}
+
